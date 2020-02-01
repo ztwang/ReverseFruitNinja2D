22d75799422a2ef116e95a8e44740e9e9a088189
Zhetao Wang <wztprivate@gmail.com>
2020-01-31 20:53:04 -0800

    Initial Unity project

---
+++ Assets/Scripts/SelfDestroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+    public float zThreshold = -8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z <= zThreshold)
+        {
+            Destroy(this);
+        }
+    }
+}
